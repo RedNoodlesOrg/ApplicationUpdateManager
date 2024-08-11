95f931c955a368e27816a4cc268482a78a799087
Fakeapate <imboden.samuel@protonmail.ch>
2024-08-11 18:41:40 +0200

    Format

--- Invoke-ApplicationUpdate.ps1
+++ Invoke-ApplicationUpdate.ps1
@@ -12,11 +12,11 @@ $apps = Get-Content -Path .\apps.json -Raw | ConvertFrom-Json
 $apps_intune = Get-WtWin32Apps
 
 # Loop trough apps in library
-foreach($app in $apps) {
+foreach ($app in $apps) {
     $in_app = $apps_intune | Where-Object PackageId -eq $app.Name
    
     # If app is not in Intune, then deploy it
-    if($null -eq $in_app){
+    if ($null -eq $in_app) {
         [PSCustomObject]@{
             Name   = $app.Name
             Status = "NOT FOUND"
@@ -24,9 +24,9 @@ foreach($app in $apps) {
         $result = New-WtWingetPackage -PackageId $app.Name -PackageFolder .\packages | Deploy-WtWin32App
         Update-WtIntuneApp -AppId $result.Id -Categories $app.Category -AvailableFor $app.AvailableFor
     }
-    else{
+    else {
         # If app is in Intune and has an update available, then deploy it and supersed it
-        if($in_app.IsUpdateAvailable -eq $true){
+        if ($in_app.IsUpdateAvailable -eq $true) {
             [PSCustomObject]@{
                 Name   = $in_app.PackageId
                 Status = "UPDATE AVAILABLE"
@@ -44,7 +44,7 @@ foreach($app in $apps) {
 
 # Remove superseded apps
 $old_apps = Get-WtWin32Apps -Superseded $true
-foreach($old_app in $old_apps){
+foreach ($old_app in $old_apps) {
     Remove-WtWin32App -AppId $old_app.GraphId
 }
 
