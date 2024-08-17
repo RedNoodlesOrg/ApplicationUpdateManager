c1e037141d9917cb802fd566702a472b65e417c5
Fakeapate <imboden.samuel@protonmail.ch>
2024-08-17 22:56:51 +0200

    Add Error Handling

--- Invoke-ApplicationUpdate.ps1
+++ Invoke-ApplicationUpdate.ps1
@@ -8,33 +8,78 @@ $apps = Get-Content -Path .\apps.json -Raw | ConvertFrom-Json
 # Apps in Intune
 $apps_intune = Get-WtWin32Apps
 
+function Write-Status ($Status) {
+    switch ($status.Result) {
+        "OK" {
+            Write-Verbose -MessageData $status.Message
+            Write-Host "$($status.Name): $($status.AppStatus)"
+        }
+        "ERROR" { 
+            Write-Error -MessageData $status.Message
+            Write-Host "$($status.Name): ERROR"
+        }
+        Default { Write-Error -Message "Unspecified Status" }
+    }
+}
+
 # Loop trough apps in library
 foreach ($app in $apps) {
     $in_app = $apps_intune | Where-Object PackageId -eq $app.Name
    
     # If app is not in Intune, then deploy it
     if ($null -eq $in_app) {
-        [PSCustomObject]@{
-            Name   = $app.Name
-            Status = "NOT FOUND"
+        $status = [PSCustomObject]@{
+            Name      = $app.Name
+            AppStatus = "NOT FOUND"
+            Result    = $null
+            Message   = $null
         }
-        $result = New-WtWingetPackage -PackageId $app.Name -PackageFolder .\packages | Deploy-WtWin32App
-        Update-WtIntuneApp -AppId $result.Id -Categories $app.Category -AvailableFor $app.AvailableFor
+        try {
+            $deployed = New-WtWingetPackage -PackageId $app.Name -PackageFolder .\packages | Deploy-WtWin32App
+            $result = Update-WtIntuneApp -AppId $deployed.Id -Categories $app.Category -AvailableFor $app.AvailableFor
+            $status.Message = $result
+            $status.Result = "OK"
+        }
+        catch {
+            $status.Message = $_
+            $status.Result = "ERROR"
+        }
+        finally {
+            Write-Status -Status $status
+        }
+        
     }
     else {
         # If app is in Intune and has an update available, then deploy it and supersed it
         if ($in_app.IsUpdateAvailable -eq $true) {
             [PSCustomObject]@{
-                Name   = $in_app.PackageId
-                Status = "UPDATE AVAILABLE"
+                Name      = $in_app.PackageId
+                AppStatus = "UPDATE AVAILABLE"
+                Result    = $null
+                Message   = $null
             }
-            New-WtWingetPackage -PackageId $app.Name -PackageFolder .\packages | Deploy-WtWin32App -GraphId $in_app.GraphId
+            try {
+                $result = New-WtWingetPackage -PackageId $app.Name -PackageFolder .\packages | Deploy-WtWin32App -GraphId $in_app.GraphId
+                $status.Message = $result
+                $status.Result = "OK"
+            }
+            catch {
+                $status.Message = $_
+                $status.Result = "ERROR"
+            }
+            finally {
+                Write-Status -Status $status
+            }
+            
         }
         else {
-            [PSCustomObject]@{
-                Name   = $in_app.PackageId
-                Status = "CURRENT"
+            $status = [PSCustomObject]@{
+                Name      = $in_app.PackageId
+                AppStatus = "CURRENT"
+                Result    = "OK"
+                Message   = $null
             }
+            Write-Status -Status $status
         }
     }
 }
